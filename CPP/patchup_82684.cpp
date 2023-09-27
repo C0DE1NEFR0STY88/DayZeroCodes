4fee894e168212b8965d38e9628841741ae1aeef
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2023-09-27 15:31:34 +0530

    Update and rename patchup(?)_82684.cpp to patchup_82684.cpp

--- CPP/patchup_82684.cpp
+++ CPP/patchup_82684.cpp
@@ -22,7 +22,7 @@ int main()
     double arr[randomman];
     
     for(int i=0;i<randomman;i++)
-        arr[i]=rand()%10000;
+        arr[i]=rand()%10000; //see actual for updates on this one
     
     numele=sizeof(arr)/sizeof(arr[0]);
     cout<<"\nThe array has "<<numele<<" elements, with each element taking "<<sizeof(arr[0])<<" bytes of storage.";
