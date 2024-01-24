09cb408c73c6caf36595795d112dc6bc5dc42fd7
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2024-01-24 13:29:30 +0530

    Update toolkit_megacomp.py
    JAI SHREE RAM

--- PY/toolkit_megacomp.py
+++ PY/toolkit_megacomp.py
@@ -1,15 +1,24 @@
 #mega comp
 
-#tech values=> input arr;list;sort();reversed()
+#tech_vals=>input arr;list;sort();reversed();key;random();if;while;sleep();seed();for;platform.system();os.system();
+            
 import random
 import time
 import os
+import platform
 
 def randomfunc():
     random.seed(time.time())
     for i in range(20):
         print(random.random())
-        time.sleep(1)
+        time.sleep(1) #in seconds
+
+def clearscreen():
+    if(platform.system()=='Windows'):
+        os.system('cls')
+    elif(platform.system()=='Linux'):
+        os.system('clear')
+        #Windows: os.name == 'nt'; Mac/Linux/BSD: os.name == 'posix'
 
 
 #mainline
@@ -34,15 +43,33 @@ print("Revresed() impact: ",str_list_rev)
 str_list.sort(key=len) #sort() using a KEY value; will sort based on length of each string
 print("Sorted using key val(len): ",str_list) #if multiple same size, then it defaults to ASCII val
 
-#https://www.geeksforgeeks.org/sort-in-python/
-#https://www.geeksforgeeks.org/python-reversed-function/
-
-#https://www.geeksforgeeks.org/python-random-module/
+print("\n")
 
 random_choice=input("Would you like to launch the random function module? (Control will be taken away for 20 seconds) (Y/N): ")
 if(random_choice=='Y' or random_choice=='y'):
     randomfunc()
+    loopflag=1
+    while loopflag==1:
+        random_choice_repeat=input("Launch random () module again? (Would clear screen) (Y/N)?: ")
+        if(random_choice_repeat=='Y' or random_choice_repeat=='y'):
+            clearscreen() #userdefined
+            randomfunc()
+        elif(random_choice_repeat=='N' or random_choice_repeat=='n'):
+            loopflag=0
+            break
+        else:
+            print("Incorrect choice.")
+print("Random segment completed.")
 
 
+
+
+#https://www.geeksforgeeks.org/sort-in-python/
+#https://www.geeksforgeeks.org/python-reversed-function/
+#https://www.geeksforgeeks.org/python-random-module/
 #https://stackoverflow.com/questions/27276135/python-random-system-time-seed
 #https://www.programiz.com/python-programming/time/sleep
+#https://docs.python.org/3/howto/curses.html
+#https://docs.python.org/3/library/platform.html#module-platform
+#https://www.geeksforgeeks.org/clear-screen-python/
+#https://stackoverflow.com/questions/110362/how-can-i-find-the-current-os-in-python
