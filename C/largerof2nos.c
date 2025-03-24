c24428b5a7ce61e018138ba79b44e50d7633e6dd
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2025-03-24 15:48:07 +0530

    Update largerof2nos.c

--- C/largerof2nos.c
+++ C/largerof2nos.c
@@ -17,10 +17,19 @@ void main()
     
     if(a>b)
         printf("The number %d is greater.", a);
-    else if(a<b)
+    else if(a<b) //this has to be else if, otherwise 2 -4 (and where 1st is larger) would print else statement as well since 1st if is dangling, else gets attached to 2nd if
         printf("The number %d is greater.", b);
     else
         printf("Both numbers are equal.");
+    
+    /*
+    if(a>b)
+        printf("The first num %d is larger", a);
+    if(b>a) //this has to be else if, otherwise 2 -4 (and where 1st is larger) would print else statement as well since 1st if is dangling, else gets attached to 2nd
+        printf("The second num %d is larger",b);
+    else
+        printf("The nos %d and %d are equal", a, b);
+    */
     
     //getch();
 }
