9a70b1b8da5081fd0d47f9e5a494dd6dd9ebd89e
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2025-03-25 08:27:59 +0530

    Create graphicsincZEPHYR.c

---
+++ C/graphicsincZEPHYR.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  clrscr();
+  //https://stackoverflow.com/questions/24708700/c-detect-when-user-presses-arrow-key
+  //https://www.geeksforgeeks.org/draw-a-moving-cycle-using-computer-graphics-programming-in-c-cpp/
+  getch();
+}
