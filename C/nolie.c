8a33dd784b0ba4aba433cc0a9e6272c261a3dbb4
C0DE1NEFR0STY88 <69596141+C0DE1NEFR0STY88@users.noreply.github.com>
2025-04-15 16:38:18 +0530

    Update nolie.c

--- C/nolie.c
+++ C/nolie.c
@@ -3,7 +3,7 @@
 
 void main()
 {
-    int a=0, b=1, c, n, i=0; //c = a+b, //also valid
+    int a=0, b=1, c, n, i=0; //c = a+b, //also valid //LONG LONG UNSIGNED INT WOULD BE %lld instead of %d
     printf("Enter the range: ");
     scanf("%d", &n);
     //printf("%d", n);
@@ -25,3 +25,30 @@ void main()
 //ssl verif shutdown will expose you to MITM attack
 //https://stackoverflow.com/questions/69266522/how-ssl-certificate-prevents-man-in-the-middle-attack
 //https://www.onlinegdb.com/
+
+
+//LONG LONG METHOD with i COUNT:
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    //clrscr();
+    long long unsigned int a=0, b=1, c=a+b, n, i=0;
+    
+    printf("enter range:");
+    scanf("%lld",&n);
+    
+    printf("%lld %lld ", a, b);
+    
+    while(i<=n)
+    {
+        printf("%lld ", c);
+        a=b;
+        b=c;
+        c=a+b;
+        i++;
+    }
+    
+    //getch();
+}
